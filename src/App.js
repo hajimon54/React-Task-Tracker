c6d2496e719b2454d0ba4b67b03dcb536ee82df0
Idris Haji <idris@Idriss-MacBook-Pro.local>
2023-06-14 13:24:30 +0100

    Added a reminder function, double click will change task to true or false

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
-function App() {
+const App = () => {
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -24,12 +24,30 @@ function App() {
     },
   ]);
 
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No tasks recorded"
+      )}
     </div>
   );
-}
+};
 
 export default App;
