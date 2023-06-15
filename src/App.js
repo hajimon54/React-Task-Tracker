e4071d7380908fb883e0b425956a6f18262543a2
Idris Haji <hajimon_53@hotmail.com>
2023-06-15 15:12:57 +0100

    added function to open and close the form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -27,7 +28,11 @@ const App = () => {
 
   //Add tasks
   const addTask = (task) => {
-    console.log(task);
+    const id = Math.floor(Math.random() * 10000) + 1;
+
+    const newTask = { id, ...task };
+
+    setTasks([...tasks, newTask]);
   };
 
   //Delete Task
@@ -46,8 +51,11 @@ const App = () => {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask} />
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
